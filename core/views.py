71949ae714388445bea2abb3042da6e303dc30b9
vvact <vbm7752@gmail.com>
2025-07-31 16:17:02 +0300

    Add styled homepage using Bootstrap

--- core/views.py
+++ core/views.py
@@ -1,9 +1,5 @@
 # core/views.py
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_view(request):
-    return HttpResponse("""
-        <h1>Welcome to the Shule API 🚀</h1>
-        <p>This is the backend for Shule. Use <a href="/api/">/api/</a> to access the API.</p>
-    """)
-
+    return render(request, 'core/home.html')
